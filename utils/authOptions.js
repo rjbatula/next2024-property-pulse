2289dbeafbfeea344d817a6cd6643ec6bc67a8f7
Ralph Joshua <rjbatula@yahoo.com>
2024-03-26 10:43:43 +0800

    Saving user to DB and Session in authOptions

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from 'next-auth/providers/google'
+import connectDB from '@/config/database'
+import User from '@/models/User'
 
 export const authOptions = {
 	providers: [
@@ -16,17 +18,33 @@ export const authOptions = {
 	],
 	callbacks: {
 		// Invoked on successful sign in
-		async asignIn({ profile }) {
+		async signIn({ profile }) {
 			//1. Connect to DB
+			await connectDB()
 			//2. Check if user exist
+			const userExists = await User.findOne({ email: profile.email })
 			//3. If not, add user to DB
+			if (!userExists) {
+				// Truncate user name if too long
+				const username = profile.name.slice(0, 20)
+
+				await User.create({
+					email: profile.email,
+					username,
+					image: profile.picture,
+				})
+			}
 			//4. return true to allow sign in
+			return true
 		},
 		// Modifiy the session obj
 		async session({ session }) {
 			//1. Get user from DB
+			const user = await User.findOne({ email: session.user.email })
 			//2. Assign user id to session
+			session.user.id = user._id.toString()
 			//3. return session
+			return session
 		},
 	},
 }
