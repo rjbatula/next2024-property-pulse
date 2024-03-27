b65d5104c2f9f3c243999735746383be5f21857b
Ralph Joshua <rjbatula@yahoo.com>
2024-03-27 14:04:02 +0800

    Added property add POST API route and build the property data to submit

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,7 +1,7 @@
 import connectDB from '@/config/database'
 import Property from '@/models/Property'
 
-//GET /api/properties
+// GET /api/properties
 export const GET = async (request) => {
 	try {
 		//Connect to DB
@@ -17,5 +17,54 @@ export const GET = async (request) => {
 	} catch (error) {
 		console.log(error)
 		return new Response('Something went wrong', { status: 500 })
+	}
+}
+
+// POST /api/properties
+export const POST = async (request) => {
+	try {
+		const formData = await request.formData()
+
+		// Handle all values from amenities and images
+		const amenities = formData.getAll('amenities')
+		const images = formData
+			.getAll('images')
+			.filter((image) => image.name !== '')
+
+		// Build the property data object for DB
+		const propertyData = {
+			type: formData.get('type'),
+			name: formData.get('name'),
+			description: formData.get('description'),
+			location: {
+				street: formData.get('location.street'),
+				city: formData.get('location.city'),
+				state: formData.get('location.state'),
+				zipcode: formData.get('location.zipcode'),
+			},
+			beds: formData.get('beds'),
+			baths: formData.get('baths'),
+			square_feet: formData.get('square_feet'),
+			amenities,
+			rates: {
+				weekly: formData.get('rates.weekly'),
+				monthly: formData.get('rates.monthly'),
+				nightly: formData.get('rates.nightly'),
+			},
+			seller_info: {
+				name: formData.get('seller_info.name'),
+				email: formData.get('seller_info.email'),
+				phone: formData.get('seller_info.phone'),
+			},
+			images,
+		}
+
+		console.log(propertyData)
+
+		return new Response(JSON.stringify({ message: 'Success' }), { status: 200 })
+	} catch (error) {
+		return new Response(JSON.stringify('Failed to add property'), {
+			status: 500,
+		})
 	}
 }
