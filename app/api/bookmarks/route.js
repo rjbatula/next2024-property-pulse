5204aff20fd41ebece9fafc87d9a480f02c7eeea
Ralph Joshua <rjbatula@yahoo.com>
2024-04-01 15:56:43 +0800

    Added saved property api route and render property card on save property page

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -4,7 +4,35 @@ import Property from '@/models/Property'
 import { getSessionUser } from '@/utils/getSessionUser'
 
 export const dynamic = 'force-dynamic'
+// GET /api/bookmarks
+export const GET = async () => {
+	try {
+		await connectDB()
 
+		// Get User Id
+		const sessionUser = await getSessionUser()
+
+		// Check session
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response('User ID is required', { status: 401 })
+		}
+
+		const { userId } = sessionUser
+
+		// Find User in DB
+		const user = await User.findOne({ _id: userId })
+
+		// Get user's bookmarks
+		const bookmarks = await Property.find({ _id: { $in: user.bookmarks } })
+
+		return new Response(JSON.stringify(bookmarks), { status: 200 })
+	} catch (error) {
+		console.log(error)
+		return new Response('Something went wrong', { status: 500 })
+	}
+}
+
+// POST /api/bookmarks
 export const POST = async (request) => {
 	try {
 		await connectDB()
