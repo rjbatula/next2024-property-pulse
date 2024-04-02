9da5e3e44e2d083d66ccba94251a7378a7923163
Ralph Joshua <rjbatula@yahoo.com>
2024-04-02 15:28:05 +0800

    Added fix to auth session for sending message

--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -2,8 +2,10 @@
 import { useState } from 'react'
 import { FaPaperPlane } from 'react-icons/fa'
 import { toast } from 'react-toastify'
+import { useSession } from 'next-auth/react'
 
 const PropertyContactForm = ({ property }) => {
+	const { data: session } = useSession()
 	const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
 	const [phone, setPhone] = useState('')
@@ -33,7 +35,8 @@ const PropertyContactForm = ({ property }) => {
 				toast.success('Message sent successfully')
 				setWasSubmitted(true)
 			} else if (res.status === 400 || res.status === 401) {
-				toast.error(data.message)
+				const dataObj = res.json()
+				toast.error(dataObj.message)
 			} else {
 				toast.error('Error sending form')
 			}
@@ -52,7 +55,9 @@ const PropertyContactForm = ({ property }) => {
 		// {/* <!-- Contact Form --> */}
 		<div className='bg-white p-6 rounded-lg shadow-md'>
 			<h3 className='text-xl font-bold mb-6'>Contact Property Manager</h3>
-			{wasSubmitted ? (
+			{!session ? (
+				<p>You must be logged in to send a message</p>
+			) : wasSubmitted ? (
 				<p className='text-green-500 mb-4'>
 					You message has been sent successfully
 				</p>
